641b32187307b914a3a0a005cb0761a5ac62ef49
Yaroslav Tkachenko <sapiensy@gmail.com>
2016-11-13 18:46:53 -0800

    New S3/CF deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+rake generate
+aws s3 cp ./public/ s3://sap1ens.com/ --recursive
+aws cloudfront create-invalidation --distribution-id E1XUIQERL9QZH2 --paths /
