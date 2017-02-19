b534f0345b970ce91908b158cdcb024131ce0885
Yaroslav Tkachenko <sapiensy@gmail.com>
2017-02-19 12:29:33 -0800

    Update deploy script - use AWS profiles

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o pipefail
 
 rake generate
-aws s3 cp ./public/ s3://sap1ens.com/ --recursive
+aws s3 cp ./public/ s3://sap1ens.com/ --recursive --profile personal
 sleep 5
-aws cloudfront create-invalidation --distribution-id E1XUIQERL9QZH2 --paths "/*"
+aws cloudfront create-invalidation --distribution-id E1XUIQERL9QZH2 --paths "/*" --profile personal
